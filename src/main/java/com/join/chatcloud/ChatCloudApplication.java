5e2f6304d1cd26f5f81c9ddac9abb41b189c7e34
joinhanren <joinhanren@gmail.com>
2024-06-20 21:12:47 +0800

    初始构建项目,数据库连接依赖导入,连接基本配置

---
+++ src/main/java/com/join/chatcloud/ChatCloudApplication.java
@@ -0,0 +1,13 @@
+package com.join.chatcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChatCloudApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ChatCloudApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/join/chatcloud/ChatCloudApplicationTests.java
@@ -0,0 +1,13 @@
+package com.join.chatcloud;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChatCloudApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
